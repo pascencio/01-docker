30c71eaf69445ec048a4fb01a1952f4bff5185cf
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-12-05 01:07:45 -0300

    feat: Tarea instroducción

---
+++ tareas/01-introduccion/app.js
@@ -0,0 +1,15 @@
+async function sleep(ms){
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+    while(true) {
+        console.log("Hola mundo!");
+        console.log("Esperando 10 segundos...");
+        await sleep(1000);
+    }
+}
+
+(async () => {
+    await main();
+})();
